ed54ca62bbff2fa0074548ab31e36fb9675b5ab7
Tristan Robichaud <tristan.robichaud@outlook.com>
2023-12-11 10:44:39 -0500

    Add HomePage component and update App component

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -8,6 +8,7 @@ function Card({ imageSrc, title }) {
             <div className="cards">
                 <img src={imageSrc} alt={title} />
                 <h1>{title}</h1>
+                <p>{ import.meta.env.SOME_VAR }t</p>
             </div>
         </>
     )
