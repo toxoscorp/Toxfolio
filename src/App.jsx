9f7a78d9d45fb90446c2979781cc7b7c6fa7f549
Tristan Robichaud <tristan.robichaud@outlook.com>
2023-12-14 20:32:01 -0500

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import React from 'react'
 import './App.css'
 
-import HomePage from './views/HomePage'
-
 function App() {
-  console.log(window.location.pathname);
-
   return (
     <>
-      <div className='app'>
-        <HomePage />
-      </div>
     </>
   )
 }
