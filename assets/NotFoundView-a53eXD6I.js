1125b4afc94b22fc13e62497ddad74f61295f819
CI <CI@example.com>
2024-01-24 16:07:03 +0000

    deploy

---
+++ assets/NotFoundView-a53eXD6I.js
@@ -0,0 +1,1 @@
+import{_ as o,c as t,o as n,a as e}from"./index-JSbrehr2.js";const c={},s=e("h1",null,"404",-1),_=e("p",null,"Page not found",-1),a=[s,_];function r(d,l){return n(),t("div",null,a)}const u=o(c,[["render",r]]);export{u as default};
